0409a485058c442c8adc0585d7b771ed8034b987
StephLingard <94009859+StephLingard@users.noreply.github.com>
2024-11-05 13:08:28 -0800

    filtering detections and qa'ing data
    creating basic data sumamaries to qa data entry. Either there is a lot of noise in the acoustic detections, or there are data entry and wrangling errors as fish released downstream are being detected upstream at both the site they were released at and at alternative sites which is impossible.

--- 2_Acoustic detections.R
+++ 2_Acoustic detections.R
@@ -28,7 +28,7 @@ df$receiver.serial <- gsub(".*-","",df$Receiver)
 df$tagID <- gsub(".*-*-","",df$Transmitter)
 
 #remove false detections
-# read in tagging and relase data
+# read in tagging and release data
 
 tag.deployment <- read.csv(here("raw data", "acoustic tagging.csv"))%>%
   rename(trough.section=trough)
@@ -38,6 +38,12 @@ release.data <- read.csv(here("raw data", "acoustic fish release data.csv"))%>%
 tag.meta <- tag.deployment%>%
   full_join(., release.data, by=c("release.date", "trough.section"))%>%
   mutate(release.datetime=ymd_hm(paste(release.date, release.time, by=" ")))
+
+tag.meta %>%
+  group_by(release.location)%>%
+  summarise(length(unique(tagid)))
+
+length(unique(tag.meta$tagid)) # one ID is missing????
 
 tag.deployment.receiver <- df %>%
   filter(receiver.serial == 108656)
@@ -71,8 +77,89 @@ df3 <- df2 %>%
   arrange(., tagID, datetime.local)%>%
   mutate(diff.time = difftime(datetime.local, lag(datetime.local), units="secs"))
 
+# now recalculate time diffs
+df4 <- df4 %>% 
+  group_by(tagID)%>%
+  arrange(., tagID, datetime.local)%>%
+  mutate(diff.time = difftime(datetime.local, lag(datetime.local), units="secs"))
 
-df4 <- df3 %>% 
-  filter(diff.time >= 45)
+# check the filter worked
+min(df4$diff.time, na.rm=T)
 
+
+## Read in receiver meta ####
+
+r.meta <- read.csv(here("raw data", "receiver metadata_2024.csv"))%>%
+  select(-Way.Point)
+
+# remove "VR2W-" and "VR2Tx" from Receiver in df4
+
+df4$Receiver <- gsub("VR2W-","", df4$Receiver)
+df4$Receiver <- gsub("VR2Tx-","", df4$Receiver)
+
+# now add in location and up or downstream by merging dfs
+# also add in release.datetime and location column
+
+df5 <- df4 %>% 
+  select(-c(Station.Name, Latitude, Longitude))%>%
+  merge(., r.meta, by="Receiver")
+
+fish.dat <- tag.meta%>%
+  select(tagid, release.datetime,release.location)%>%
+  rename(tagID=tagid)
+
+df5 <- df5%>%
+  merge(., fish.dat, by="tagID")%>%
+  select(-c('Date.and.Time..UTC.',Transmitter))
+
+# remove un-needed vectors to free up memory
+rm(fish.dat)
+
+#how many IDs detected?
+
+length(unique(df5$tagID)) #292 IDs detected.
+
+## Lets first focus on Hammersley
+
+df5 %>%
+  group_by(release.location)%>%
+  summarise(length(unique(tagID)))
+
+# 102 released upstream, # 68 downstream. Need to confirm in notes this is correct
+
+ham.fish <-df5 %>%
+  filter(release.location %in% c("ham.ds", "ham.us"))
+
+ham.fish %>% 
+  group_by(release.location)%>%
+  summarise(length(unique(tagID)))
+
+# were any fish released downstream detected upstream?
+
+ham.fish %>%
+  group_by(release.location,Location)%>%
+  summarise(length(unique(tagID)))
+
+# so about 80% of fish released downstream may have made it downstream
+# but 11 fish appearantly moved upstream from downstream?
+
+up.movement <- ham.fish%>%
+  filter(release.location %in% "ham.ds" & Location %in% "upstream")
+
+up.movement%>%
+  group_by(tagID)%>%
+  summarise(n()) # there are a lot of detections of these fish
+
+min(up.movement$datetime.local)
+max(up.movement$datetime.local)
+
+#hmmm these dets occured while gates were shut. So either there is a hole in the flood gates, a lot of noise, 
+# or there are mistakes in the release records. Will have to go and check the raw data
+
+df5 %>%
+  mutate(location_site=paste(Location, Site, sep="-"))%>%
+  group_by(release.location, location_site)%>%
+  summarise(length(unique(tagID)))
+
+# there are a lot of fish detected in places theye weren't relased.
 
