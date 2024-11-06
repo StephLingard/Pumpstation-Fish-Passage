623a9f1df0aa2429bfaa6863705b8e4c7c834a49
StephLingard <94009859+StephLingard@users.noreply.github.com>
2024-11-06 14:47:42 -0800

    updated main

--- 2_Acoustic detections.R
+++ 2_Acoustic detections.R
@@ -16,6 +16,8 @@ library(tidyverse)
 library(purrr)
 library(here)
 
+
+# read in raw detections ####
 df <- list.files(path=here("raw data","acoustic receivers"), full.names=TRUE ) %>% 
   map_dfr(read.csv)
 
@@ -28,7 +30,7 @@ df$receiver.serial <- gsub(".*-","",df$Receiver)
 df$tagID <- gsub(".*-*-","",df$Transmitter)
 
 #remove false detections
-# read in tagging and release data
+# read in tagging and release data ####
 
 tag.deployment <- read.csv(here("raw data", "acoustic tagging.csv"))%>%
   rename(trough.section=trough)
@@ -49,11 +51,42 @@ length(unique(tag.meta$tagid)) # one ID is missing????
 tag.deployment.receiver <- df %>%
   filter(receiver.serial == 108656)
 
-# now filter out tag detections prior to release
+# filter out tag detections prior to release ####
 
 df2 <- df%>%
   filter(receiver.serial != 108656 & tagID %in% tag.meta$tagid & datetime.local > tag.meta$release.datetime)
 
+# now filter detections less than 45 seconds apart
+# first have to create a time.diff column
+
+df3 <- df2 %>%
+  select(-c(Sensor.Precision, Sensor.Unit, Sensor.Value, Transmitter.Name, Transmitter.Serial, Transmitter.Type))%>%
+  group_by(tagID)%>%
+  arrange(., tagID, datetime.local)%>%
+  mutate(diff.time = difftime(datetime.local, lag(datetime.local), units="secs"))
+
+# now recalculate time diffs after filtering out erroneous detections
+
+df4 <- df3 %>% 
+  group_by(tagID)%>%
+  arrange(., tagID, datetime.local)%>%
+  mutate(diff.time = difftime(datetime.local, lag(datetime.local), units="secs"))
+
+# check the filter worked
+min(df4$diff.time, na.rm=T)
+
+# One minute data filter #### 
+# Explanation: there must be a second detection with in 1 minute of the current one.
+# diff_time is the time difference between the current time and the previous record. lead diff.time is the time between obs and next obs
+
+df5 <- df4 %>%
+  group_by(tagID)%>%
+  mutate(Cond1=ifelse(diff.time <=60, TRUE, FALSE), 
+         Cond2=ifelse(lead(diff.time) <=60, TRUE, FALSE))%>%
+  filter(Cond2 == TRUE | Cond1==TRUE)
+
+
+# filter out non-study tags ####
 non.study.tags <- df%>%
   filter(receiver.serial != 108656 & !tagID %in% tag.meta$tagid )
 
@@ -67,41 +100,22 @@ summary <- non.study.tags %>%
   filter(n>1)%>%
   mutate(sensor= ifelse(tagID %in% sensors$tagID, "Yes", "No"))
 
+#write.csv(summary, file=here("data summaries","non study tags.csv"))
 
-write.csv(summary, file=here("data summaries","non study tags.csv"))
-
-# now filter detections less than 45 seconds apart
-
-df3 <- df2 %>%
-  select(-c(Sensor.Precision, Sensor.Unit, Sensor.Value, Transmitter.Name, Transmitter.Serial, Transmitter.Type))%>%
-  group_by(tagID)%>%
-  arrange(., tagID, datetime.local)%>%
-  mutate(diff.time = difftime(datetime.local, lag(datetime.local), units="secs"))
-
-# now recalculate time diffs
-df4 <- df3 %>% 
-  group_by(tagID)%>%
-  arrange(., tagID, datetime.local)%>%
-  mutate(diff.time = difftime(datetime.local, lag(datetime.local), units="secs"))
-
-# check the filter worked
-min(df4$diff.time, na.rm=T)
-
-
-## Read in receiver meta ####
+# Read in receiver meta ####
 
 r.meta <- read.csv(here("raw data", "receiver metadata_2024.csv"))%>%
   select(-Way.Point)
 
 # remove "VR2W-" and "VR2Tx" from Receiver in df4
 
-df4$Receiver <- gsub("VR2W-","", df4$Receiver)
-df4$Receiver <- gsub("VR2Tx-","", df4$Receiver)
+df5$Receiver <- gsub("VR2W-","", df5$Receiver)
+df5$Receiver <- gsub("VR2Tx-","", df5$Receiver)
 
 # now add in location and up or downstream by merging dfs
 # also add in release.datetime and location column
 
-df5 <- df4 %>% 
+df6 <- df5 %>% 
   select(-c(Station.Name, Latitude, Longitude))%>%
   merge(., r.meta, by="Receiver")
 
@@ -109,63 +123,60 @@ fish.dat <- tag.meta%>%
   select(tagid, release.datetime,release.location)%>%
   rename(tagID=tagid)
 
-df5 <- df5%>%
+fish.dat %>%
+  group_by(release.location)%>%
+  summarise(length(unique(tagID))) # 300 total
+
+df6 <- df6%>%
   merge(., fish.dat, by="tagID")%>%
   select(-c('Date.and.Time..UTC.',Transmitter))
 
-# remove un-needed vectors to free up memory
-rm(fish.dat)
+# Data exploration and QA ####
+# how many IDs detected?
 
-#how many IDs detected?
+length(unique(df6$tagID)) #287 IDs detected out of 300
 
-length(unique(df5$tagID)) #292 IDs detected.
+# Fish detected
 
-## Lets first focus on Hammersley
-
-df5 %>%
+df6 %>%
   group_by(release.location)%>%
   summarise(length(unique(tagID)))
 
-# 102 released upstream, # 68 downstream. Need to confirm in notes this is correct
-
-ham.fish <-df5 %>%
-  filter(release.location %in% c("ham.ds", "ham.us"))
-
-ham.fish %>% 
-  group_by(release.location)%>%
-  summarise(length(unique(tagID)))
+#Hammersley: 103 released upstream but 102 detected, # 105 released downstream but 98 detected
+# Hatzic: 51 released ds with 50 detected, 41 released up with 39 detected
 
 # were any fish released downstream detected upstream?
 
-ham.fish %>%
+df6 %>%
   group_by(release.location,Location)%>%
-  summarise(length(unique(tagID)))
+  summarise(length(unique(tagID))) # a few. Could be noise
 
-# so about 80% of fish released downstream may have made it downstream
-# but 11 fish appearantly moved upstream from downstream?
+# what about fish moving upstream? only interested in Hammersley because there is no upstream path (apparently)
 
-up.movement <- ham.fish%>%
-  filter(release.location %in% "ham.ds" & Location %in% "upstream")
-
-up.movement%>%
-  group_by(tagID)%>%
-  summarise(n()) # there are a lot of detections of these fish
-
-min(up.movement$datetime.local)
-max(up.movement$datetime.local)
-
-#hmmm these dets occured while gates were shut. So either there is a hole in the flood gates, a lot of noise, 
-# or there are mistakes in the release records. Will have to go and check the raw data
-
-df5 %>%
+df6 %>%
   mutate(location_site=paste(Location, Site, sep="-"))%>%
   group_by(release.location, location_site)%>%
   summarise(length(unique(tagID)))
 
-# there are a lot of fish detected in places they weren't released. Fish release downstream of each pump
-# shouldn't be upstream of pumps.
+up.movement <- df6%>%
+  mutate(location_site=paste(Location, Site, sep="-"))%>%
+  filter(release.location %in% "ham.ds" & location_site %in% "upstream-mountain slough")
 
-dat_qa <- df5 %>%
+up.movement%>%
+  ggplot(., aes(x=datetime.local))+
+  geom_histogram()
+
+up.movement%>% 
+  group_by(tagID)%>%
+  summarise(n()) # there are a lot of detections of some of these fish
+
+upID <- up.movement%>%
+  group_by(tagID)%>%
+  filter(row_number()==1)
+
+## Data QA and Clean Up ####
+
+dat_qa <- df6 %>%
   mutate(location_site=paste(Location, Site, sep="-"))%>%
   filter(release.location %in% c("hat.ds","ham.ds") & location_site %in% c("upstream-mountain slough", "upstream-hatzic"))
 
@@ -174,7 +185,7 @@ dat_qa %>%
   group_by(release.location, release.datetime, location_site)%>%
   summarise(length(unique(tagID)))
 
-df5 %>%
+df6 %>%
   mutate(location_site=paste(Location, Site, sep="-"))%>%
   filter(release.location %in% "hat.ds" & release.datetime %in% "2024-05-15 11:46:00" & location_site %in% "upstream-hatzic") %>%
   group_by(tagID)%>%
@@ -187,7 +198,50 @@ dat_qa %>%
   filter(release.location %in% "hat.ds" & location_site %in% "upstream-hatzic")%>%
   summarise(unique(Receiver)) # this is the only upstream receiver so it seems fish went upstream!
 
+
 ## Plot Detection Histories for each fish ####
 # start with numbering receivers at each location. Ham.us=1, ham.sd=2, hat.us=3, hat.ds=4. 
 # add row for release at start.
 # plot the release group that was detected upstream of hatzic first (17 fish). 
+
+### Adding in release data as a row in the data frame for plotting and analysis ####
+
+fish.dat$release.location <- as.factor(fish.dat$release.location)
+fish.dat$location_site <- fish.dat$release.location
+
+levels(fish.dat$location_site) <- forcats::fct_recode(fish.dat$location_site, 
+                                                         'upstream-mountain slough'="ham.us",
+                                                         'downstream-mountain slough'="ham.ds",
+                                                         'upstream-haztic'="hat.us",
+                                                         'downstream-hatzic'="hat.ds")
+release.df <- fish.dat%>%
+  mutate(Receiver=NA,
+         datetime.local=release.datetime, 
+         location=release.location)
+
+plot.df <- df6%>%
+  mutate(location_site=paste(Location, Site, sep="-"))%>%
+  select(tagID, location_site, datetime.local, release.location, 
+         release.datetime, Receiver)%>%
+  bind_rows(., release.df)%>%
+  group_by(tagID)%>%
+  arrange(datetime.local)%>%
+  mutate(location_site=as.factor(location_site), 
+         location=location_site)
+
+
+levels(plot.df$location) <- fct_recode(plot.df$location, 
+                                            '1'="upstream-mountain slough",
+                                            '2'="downstream-mountain slough",
+                                            '3'="upstream-haztic",
+                                            '4'="downstream-hatzic")
+
+
+plot.df %>%
+  filter(tagID %in% up.movement$tagID)%>%
+  ggplot(., aes(x=datetime.local, y=location_site))+
+  geom_point()+
+  geom_line()+
+  facet_wrap(~tagID)
+
+  
