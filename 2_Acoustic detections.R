349610674d1a5f318f22097ac37029192f60bd68
StephLingard <94009859+StephLingard@users.noreply.github.com>
2024-12-02 14:53:33 -0800

    fraser data added

--- 2_Acoustic detections.R
+++ 2_Acoustic detections.R
@@ -43,7 +43,7 @@ tag.meta <- tag.deployment%>%
          release.date=ymd(release.date))
 
 tag.meta %>%
-  group_by(release.date, release.location)%>%
+  group_by(release.location)%>%
   summarise(length(unique(tagid)))
 
 length(unique(tag.meta$tagid)) # one ID is missing????
@@ -132,12 +132,12 @@ df6 <- df6%>%
   mutate(location_site=paste(Location, Site, sep="-"))%>%
   merge(., fish.dat, by="tagID")
 
-# Remove tags that aren't detected at least twice in the same "location-site" with in 1 hour ####
+# Remove tags that aren't detected at least twice in the same region with in 1 hour ####
 
 df7 <- df6 %>%
-  group_by(tagID)%>%
-  mutate(cond1 = ifelse(lead(diff.time) > 3600, FALSE, TRUE), 
-         cond2= ifelse(diff.time > 3600, FALSE, TRUE))%>%
+  group_by(tagID, location_site)%>%
+  mutate(cond1 = ifelse(lead(diff.time) > 1800, FALSE, TRUE), 
+         cond2= ifelse(diff.time > 1800, FALSE, TRUE))%>%
   filter(cond1 | cond2 == TRUE)
 
 # Data exploration and QA ####
@@ -156,7 +156,7 @@ df7 %>%
 # were any fish released downstream detected upstream?
 
 df7 %>%
-  group_by(release.location,Location)%>%
+  group_by(release.location, location_site)%>%
   summarise(length(unique(tagID))) # a few. Could be noise
 
 # what about fish moving upstream? only interested in Hammersley because there is no upstream path (apparently)
@@ -175,16 +175,13 @@ up.movement%>%
   facet_wrap(~tagID)
 
 # there are a lot of detections of some of these fish and the filters are very conservative
-
 upIDs <- up.movement%>%
   group_by(tagID)%>%
   filter(row_number()==1)
 
 ## Data QA and Clean Up ####
-
 dat_qa <- df7 %>%
   filter(release.location %in% c("hat.ds","ham.ds") & location_site %in% c("upstream-mountain slough", "upstream-hatzic"))
-
 
 dat_qa %>%
   group_by(release.location, release.datetime, location_site)%>%
@@ -212,8 +209,10 @@ levels(fish.dat$location_site) <- forcats::fct_recode(fish.dat$location_site,
                                                          'downstream-mountain slough'="ham.ds",
                                                          'upstream-hatzic'="hat.us",
                                                          'downstream-hatzic'="hat.ds")
+
+
 release.df <- fish.dat%>%
-  mutate(Receiver=NA,
+  mutate(Receiver="release",
          datetime.local=release.datetime, 
          location=release.location)
 
@@ -225,29 +224,158 @@ plot.df <- df7%>%
   group_by(tagID)%>%
   arrange(datetime.local)%>%
   mutate(location_site=as.factor(location_site), 
-         location=location_site)
+         location=fct_recode(location_site, "1"="upstream-mountain slough",
+                                            "2"="downstream-mountain slough",
+                                            "3"="upstream-hatzic",
+                                            "4"="downstream-hatzic"))
 
-
-levels(plot.df$location) <- fct_recode(plot.df$location, 
-                                            '1'="upstream-mountain slough",
-                                            '2'="downstream-mountain slough",
-                                            '3'="upstream-hatzic",
-                                            '4'="downstream-hatzic")
+plot.df$Receiver <- factor(plot.df$Receiver, levels=c("release","108647","101503","10866","108661","108658","108654"))
 
 
 up.fish <- plot.df %>%
   filter(tagID %in% up.movement$tagID)%>%
-  mutate(location=as.numeric(location))%>%
+  ggplot(., aes(x=datetime.local, y=Receiver))+
+  geom_point()+
+  geom_line()+
+  facet_wrap(~tagID, scales="free_x")
+
+ggsave(up.fish, file=here("figures","Hammersley Upstream Movement.png"),
+       width=9, height=8)
+
+# all the fish detected at the same time up and down were detected on the receiver ds of flood gates. 
+
+test <- df7 %>%
+  ungroup()%>%
+  filter(Receiver!=108660)%>%
+  summarise(length(unique(tagID))) # doesn't delete any tag IDS to remove this receiver.
+
+# remove the receiver from the analysis
+df8 <- df7%>%
+  filter(Receiver!=108660)
+
+# read in Fraser Data ####
+
+fish.rel.dat <- fish.dat %>% 
+  select(tagID, release.location)
+
+fraser.dets <- read.csv(here("raw data","Detections_DI_JS_Fraser_Lingard_2024_30Nov2024.csv"))%>%
+  select(tag_id, receiver_sn, detection_datetime, subarray)%>%
+  rename(tagID=tag_id,
+         Receiver=receiver_sn, 
+         datetime.local=detection_datetime,
+         location_site=subarray)%>%
+  mutate(datetime.local=ymd_hm(datetime.local), 
+         Site="fraser", 
+         tagID=as.integer(tagID))%>%
+  merge(., fish.rel.dat, by="tagID")
+
+# array up to downstream 
+
+all.dets <- df8%>%
+  mutate(tagID=as.integer(tagID), Receiver=as.integer(Receiver))%>%
+  select(tagID, datetime.local,Receiver, location_site, Site, release.location)%>%
+  bind_rows(., fraser.dets)
+
+ham.dets <- all.dets%>%
+  filter(release.location %in% c("ham.ds", "ham.us"))%>%
+  group_by(tagID)%>%
+  arrange(datetime.local)
+
+ham.dets %>%
+  ungroup()%>%
+  filter(release.location %in% c("ham.ds", "ham.us"))%>%
+  summarise(length(unique(tagID)))# 196 detected
+
+#locations <- hammersley>mission>derby>barnston>portman>new west
+
+
+ham.dets$location_site <- factor(ham.dets$location_site, 
+                                      levels=c("release",
+                                        "upstream-mountain slough",
+                                      "downstream-mountain slough",
+                                      "downstream-hatzic",
+                                      "MISSION",
+                                      "DERBY",
+                                      "BARNSTON",
+                                      "PORT_MANN",
+                                      "NEW_WEST"))
+
+ham.plots1 <- ham.dets%>%
   ggplot(., aes(x=datetime.local, y=location_site))+
   geom_point()+
   geom_line()+
-  facet_wrap(~tagID)
+  facet_wrap_paginate(~tagID, scales="free_x",
+             nrow=4, ncol=4, page=1)
+
+ham.plots2 <- ham.dets%>%
+  ggplot(., aes(x=datetime.local, y=location_site))+
+  geom_point()+
+  geom_line()+
+  facet_wrap_paginate(~tagID, scales="free_x",
+                      nrow=4, ncol=4, page=2)
+
+ham.plots3 <- ham.dets%>%
+  ggplot(., aes(x=datetime.local, y=location_site))+
+  geom_point()+
+  geom_line()+
+  facet_wrap_paginate(~tagID, scales="free_x",
+                      nrow=4, ncol=4, page=3)
 
 
-ggsave(up.fish, file=here("figures","Hammersley Upstream Movement.png"),
-                          width=9, height=8)
+ham.plots4 <- ham.dets%>%
+  ggplot(., aes(x=datetime.local, y=location_site))+
+  geom_point()+
+  geom_line()+
+  facet_wrap_paginate(~tagID, scales="free_x",
+                      nrow=4, ncol=4, page=4)
   
-plot.df %>%
-  filter(tagID %in% up.movement$tagID)%>%
-  group_by(Receiver)%>%
-  summarise(n())
+ham.plots5 <- ham.dets%>%
+  ggplot(., aes(x=datetime.local, y=location_site))+
+  geom_point()+
+  geom_line()+
+  facet_wrap_paginate(~tagID, scales="free_x",
+                      nrow=4, ncol=4, page=5)   
+
+
+## How many fish passed hammersley? #####
+
+up.stream_surv <- ham.dets %>% 
+  ungroup()%>%
+  filter(release.location %in% "ham.us" & Site %in% "fraser") %>% 
+  summarise(length(unique(tagID))) #33 out of 105
+
+down.stream_surv <-  ham.dets %>% 
+  ungroup()%>%
+  filter(release.location %in% "ham.ds" & Site %in% "fraser") %>% 
+  summarise(length(unique(tagID)))
+
+dets_location <- ham.dets %>%
+  group_by(release.location, tagID, Site)%>%
+  summarise(n=n())
+
+detected.in.fraser <- ham.dets %>%
+  group_by(tagID, location_site)%>%
+  summarise(n= n())%>%
+  filter(n > 1)%>%
+  ungroup()%>%
+  filter(!location_site %in% c("upstream-mountain slough", "downstream-mountain slough"))%>%
+  merge(., fish.rel.dat, by="tagID")%>%
+  group_by(release.location)%>%
+  summarise(length(unique(tagID)))
+
+# how man sites in the fraser for each fish - will influence CJS mdoels
+
+ham.dets %>%
+  group_by(tagID)%>%
+  filter(Site %in% "fraser")%>%
+  summarise(n=length(unique(location_site)))%>%
+  filter(n==1)
+  
+
+summary <- ham.dets %>%
+  ungroup()%>%
+  group_by(location_site,release.location)%>%
+  summarise(n=length(unique(tagID)))%>%
+  spread(release.location,value = n)
+
+write.csv(summary, file=here("data summaries", "very rough detection summary.csv"))  
