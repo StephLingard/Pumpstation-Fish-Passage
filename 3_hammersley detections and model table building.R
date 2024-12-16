69af35ebada1270d4510bc4cb414ddb5472080fb
StephLingard <94009859+StephLingard@users.noreply.github.com>
2024-12-16 10:05:08 -0800

    figures for pressy

--- 3_hammersley detections and model table building.R
+++ 3_hammersley detections and model table building.R
@@ -11,12 +11,16 @@ library(marked)
 # Distance between sites for travel times: 
 # mountain slough = rkm 110, hatzic=72, Mission=67, Derby = 45, Barnston = 38, PORTMANN=30
 # NEWWEST= 23
+# have to remove detections of fish that have traveled downstream or were released downstream of the mountain slough upstream 
+#  pump as the flood gates transmitted the signals and it creates false fast travel times.
+
 
 rkm <- c(110.2,110.1,72,67,45,38,30,23)
-location_site <-c("upstream-mountain slough","downstream-mountain slough","downsrtream-hatzic","MISSION",
+location_site <-c("upstream-mountain slough","downstream-mountain slough","downstream-hatzic","MISSION",
                   "DERBY","BARNSTON","PORT_MANN","NEW_WEST")
 
-site_rkm <- tibble(rkm, location_site)
+site_rkm <- tibble(rkm, location_site)%>%
+  mutate(rkm=as.numeric(rkm))
 
 # read in dets and add rkm column
 dets <- read_csv(here("cleaned data","cleaned detections from hammersley.csv"),show_col_types = FALSE)%>%
@@ -27,24 +31,29 @@ dets <- read_csv(here("cleaned data","cleaned detections from hammersley.csv"),s
   mutate(time_prev=difftime(datetime.local, lag(datetime.local), units="secs"),
          transition=ifelse(location_site == lag(location_site),F,T),
          prev_rkm=lag(rkm),
-         dist_traveled=abs(rkm-lag(rkm)),
-         speed_bodyL_secs=(dist_traveled/0.0012)/as.numeric(time_prev),
-         speed_km_day=dist_traveled/(as.numeric(time_prev)/86400))
-
-test.speed <- dets %>%
-  filter(speed_bodyL_secs>1)
+         next_rkm=lead(rkm),
+         time_next=difftime(lead(datetime.local), datetime.local, units="secs"),
+         dist_traveled=lag(rkm)-rkm)
+  
 
 test.dir <- dets %>%
-  mutate(next_rkm=lead(rkm),
-    upstream_movement=ifelse(next_rkm > rkm, TRUE, FALSE))%>%
-  filter(upstream_movement==TRUE)
+  filter(next_rkm > rkm)
 
-# need to remove these two detections
+# need to remove dets of fish upstream of pump once they have gone through or been released ds
+# then need to recalculate everything
 
 dets2 <- dets %>%
-  mutate(next_rkm=lead(rkm),
-         upstream_movement=ifelse(next_rkm > rkm, TRUE, FALSE))%>%
-  filter(!upstream_movement %in% TRUE)
+  filter(dist_traveled >= 0)%>%
+  mutate(time_prev=difftime(datetime.local, lag(datetime.local), units="secs"),
+         transition=ifelse(location_site == lag(location_site),F,T),
+         prev_rkm=lag(rkm),
+         next_rkm=lead(rkm),
+         time_next=difftime(lead(datetime.local), datetime.local, units="secs"),
+         dist_traveled=lag(rkm)-rkm,
+         speed_bodyL_secs=(dist_traveled/0.0012)/as.numeric(time_prev),
+         time_diff_days=as.numeric(time_prev)/(60*60*24),
+         speed_km_day=dist_traveled/time_diff_days)
+
   
 speed_distribution <- dets2%>%
   filter(speed_bodyL_secs > 0.0000)%>%
@@ -52,21 +61,26 @@ speed_distribution <- dets2%>%
   geom_histogram()+
   labs(x="Speed (body lengths per second)")
 
-ggsave(speed_distribution, file=here("figures","travel speeds.png"),
-       width=6, height=8)
-
 speed_km_per_day <- dets2%>%
-  filter(speed_km_day > 0.0000)%>%
+  mutate(area = ifelse(rkm %in% c(NA,110.2,110.1) & next_rkm %in% c(NA,110.1,110.2),"Pump","Fraser"))%>%
+  filter(speed_km_day > 0)%>%
   ggplot(., aes(x=speed_km_day))+
   geom_histogram()+
-  labs(x="Speed (km per day)")
+  labs(x="Speed (km per day)")+
+  scale_fill_grey()+
+  theme_classic()+
+  theme(axis.text = element_text(size=12),
+        axis.title=element_text(size=14))+
+  facet_wrap(~area, scales="free_y")
 
 ggsave(speed_distribution, file=here("figures","travel speeds.png"),
        width=6, height=8)
 
 ggsave(speed_km_per_day, file=here("figures","travel speeds km per day.png"),
-       width=6, height=8)
-  
+       width=8, height=7)
+
+
+
 # travel times look okay! moving on to CJS models
 
 # Make CJS model table ####
@@ -104,10 +118,8 @@ cjs_table_ham <- dets_wide%>%
 
 cjs_table_ham$release <-  fct_recode(cjs_table_ham$release, 
                                      '1'="ham.us",'0'="ham.ds")
-
 cjs.m1 <- crm(cjs_table_ham, groups="release")
 cjs.m1 <- cjs.hessian(cjs.m1)
-
 plogis(cjs.m1$results$beta$Phi)
 
 # getting more complicated
@@ -147,7 +159,6 @@ plogis(ham.models[[3]]$results$beta$p)
 # Try with RMark ####
 library(RMark)
 
-
 test <- process.data(data = cjs_table_ham,
              model = "CJS", groups="release")
 
@@ -163,7 +174,6 @@ phi.time <- list(formula=~time)
 p.time <- list(formula =~time)
 p<- list(formual=~1)
 
-
 release.model <- mark(test, ddl,
                       model.parameters=list(Phi=phi.release))
 
@@ -171,11 +181,35 @@ estimates <- plogis(release.model$results$beta$estimate)
 lcl <- plogis(release.model$results$beta$lcl)
 ucl <- plogis(release.model$results$beta$ucl)
 params <- c("Phi:(Intercept)","Phi:release1 ","p:(Intercept)")
+params_plot <- c("Control","Archimedes","p")
 
-results.df <- data.frame(estimates, lcl, ucl, params)
+results.df <- data.frame(estimates, lcl, ucl, params, params_plot)
 
-results.df %>% 
-  filter(!params %in% p:(Intercept))%>%
-  ggplot(., aes(x=params, y=estimates))+
+prelim <- results.df %>% 
+  filter(!params %in% "p:(Intercept)")%>%
+  ggplot(., aes(x=params_plot, y=estimates))+
   geom_point()+
-  geom_errorbar(aes(x=params, ymin=lcl, ymax=ucl))
+  geom_errorbar(aes(x=params_plot, ymin=lcl, ymax=ucl))+
+  theme_classic()+
+  labs(x="Treatment", y="Survival (Phi)")+
+  theme(axis.title = element_text(size=14),
+        axis.text= element_text(size=12),
+        panel.grid = element_blank())
+
+ggsave(prelim, file=here("figures","prelim survival estimates.png"),
+       width=8, height=7)
+
+# Time in each location ####
+
+first_last <- dets2 %>%
+  group_by(tagID, rkm)%>%
+  arrange(datetime.local)%>%
+  summarise(first_det=first(datetime.local),
+            last_det=last(datetime.local),
+            duration=difftime(last_det,first_det,  units="mins"))%>%
+  arrange(.by_group = TRUE, desc(rkm))
+
+first_last
+  
+  
+  
